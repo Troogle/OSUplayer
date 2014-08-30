e1c8821668f6039d09061ba895b6ed4d859716d5
Troogle <sqh1994@163.com>
2014-08-30 20:58:15 +0800

    Ver 5.60

--- OSU player/OSU player/Core.cs
+++ OSU player/OSU player/Core.cs
@@ -237,7 +237,7 @@ namespace OSUplayer
             {
                 Settings.Default.Upgrade();
                 Settings.Default.Save();
-                if (File.Exists("list.db")) File.Delete("list.db");
+                //if (File.Exists("list.db")) File.Delete("list.db");
             }
             if (Settings.Default.QQuin == "0")
             {
